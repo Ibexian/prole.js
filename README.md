4f4a12ae7c96a12b582d6c9daa9ee86c2017ea90
Will Kamovitch <wkamovitch@mediamath.com>
2017-03-28 12:04:33 -0400

    Adding in promises and deduping some code

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Prol.js - power to the workers
 Prol.js is an automated server cacher and stub using Service Workers
 
 ### Build Instructions
+`nvm use`
+
 `npm install -g bower`
 
 `npm install && bower install`
@@ -10,8 +12,10 @@ Prol.js is an automated server cacher and stub using Service Workers
 `node prol.js`
 
 ### Prol Commands
-`record`
+`record` - Installs service worker and records API server responses while you use the site
 
-`write <outputFile>`
+`write <outputName>` - Stops any ongoing caching and saves the results to `<outputName>.json`
 
-`serve <cacheFile>`
+`clean` - Stops any ongoing caching without writing to file
+
+`serve <cacheFile>` - Installs service worker and serves previously cached results from `<cacheFile>`
--- prol.js
+++ prol.js
@@ -7,17 +7,33 @@ var browser;
 
 //Current Defaults
 const targetDir = '/Users/wkamovitch/Sites/compass/src/js';
+const defaultUrl = 'http://localhost:9000/';
 
 ncp.limit = 16;
 
 var copyDir = function(source, destination) {
-  //TODO Check that source exists
-  ncp(source, destination, function (err) {
-   if (err) {
-     return console.error(err);
-   }
-   console.log(source + " moved");
+  return new Promise(function(resolve, reject){
+    fs.stat(__dirname + source, function(err, stat) {
+      if (!err) {
+        ncp(__dirname + source, targetDir + destination, function (err) {
+         if (err) { reject(err); }
+         resolve();
+        });
+      } else {
+        reject(`Issue with ${source} (${err.code})`);
+      }
+    });
   });
+};
+
+var createOpenBrowser = function(address, message) {
+  require('chromedriver');
+  browser = new webdriver.Builder()
+    .usingServer()
+    .withCapabilities({'browserName': 'chrome' })
+    .build();
+  browser.get(address)
+    .then(console.log(message));
 };
 
 vorpal
@@ -25,40 +41,36 @@ vorpal
   //.option()
   .action(function(args, callback) {
     var self = this;
-    //binding serviceWorker
+    //TODO binding serviceWorker
       // selenium-webdriver to execute JS and bind serviceWorker that way
         // Unbind any already registered service workers
         // then register the one we want navigator.serviceWorker.register('/sw.js');
     return this.prompt({ //https://www.npmjs.com/package/inquirer
       type: 'input',
       name: 'targetURL',
-      default: 'http://localhost:9000/',
+      default: defaultUrl,
       message: 'What\'s the target url to record? ',
     }, function(result){
       self.log("Binding to " + targetDir);
       //Move files and folders to target directory (config, ask, or default)
-      copyDir(__dirname + '/workers/sw.js', targetDir + '/sw.js');
-      copyDir(__dirname + '/swDeps', targetDir  + '/swDeps');
-      //open window - probably need to close any existing web driver instances
-      require('chromedriver');
-      browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
-      browser.get(result.targetURL);
-      self.log("Server recording");
-      // callback();
+      Promise.all([copyDir('/workers/sw.js','/sw.js'), copyDir('/swDeps', '/swDeps')]).then(() => {
+        console.log("Files Moved");
+        //open window - probably need to close any existing web driver instances
+        createOpenBrowser(result.targetURL, "Server recording");
+      }, reason => { //if the promises fail don't try to open the browser
+        vorpal.exec('clean');
+        console.log(reason);
+        callback();
+      });
     });
-  })
-  .cancel(function () { //This is not yet working
-    this.log('Closing chromedriver and removing service workers');
-    browser.quit();
   });
 
 vorpal
-  .command('write <outputFile>', 'Stops any ongoing caching and saves the results') //This might need to happen inside record
+  .command('write <outputName>', 'Stops any ongoing caching and saves the results') //This might need to happen inside record
   .action(function(args, callback) {
-    //Access indexedDb in through selenium
-    //Terminate Service worker through selenium "executescript"
+    //TODO Access indexedDb in through selenium
     //save contents to file => fs.writefile(outputFile, content);
-    var out = args.outputFile + ".json";
+    var out = args.outputName + ".json";
     fs.writeFile(out, 'Hello Node.js', (err) => {
       if (err) throw err;
       console.log('It\'s saved!');
@@ -72,7 +84,7 @@ vorpal
   vorpal
     .command('clean', 'Stops any ongoing caching without writing to file') //This might need to happen inside record
     .action(function(args, callback) {
-      //Terminate Service worker through selenium "executescript"
+      //TODO Terminate Service worker through selenium "executescript"
       //close window/instance
       try {
         browser.quit();
@@ -91,23 +103,25 @@ vorpal
     return this.prompt({ //https://www.npmjs.com/package/inquirer
       type: 'input',
       name: 'targetURL',
-      default: 'http://localhost:9000/',
-      message: 'What\'s the target url to record? ',
+      default: defaultUrl,
+      message: 'What\'s the target url for serving the cache? ',
     }, function(result){
       self.log("Binding to " + targetDir);
-      //Move files and folders to target directory (config, ask, or default) - This is a duplicate action of 'record' - pull this out
-      copyDir(__dirname + '/workers/sw.js', targetDir + '/swCache.js'); //copy main service worker
-      copyDir(__dirname + '/' + args.cacheFile , targetDir + '/' + args.cacheFile); //caching file
-      copyDir(__dirname + '/swDeps', targetDir  + '/swDeps'); //dependencies
-      //open window
-      require('chromedriver');
-      browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
-      browser.get(result.targetURL);
-      self.log("Serving cache from " + args.cacheFile);
-      // callback();
+      //TODO Verify files actually moved else stop trying
+      //Move files and folders to target directory (config, ask, or default)
+      Promise.all([copyDir('/workers/swCache.js', '/swCache.js'), copyDir('/' + args.cacheFile , '/' + args.cacheFile), copyDir('/swDeps', '/swDeps')])
+        .then(() => {
+          console.log("Files Moved");
+          //open window - probably need to close any existing web driver instances
+          createOpenBrowser(result.targetURL, "Serving cache from " + args.cacheFile);
+        }, reason => {
+          vorpal.exec('clean');
+          console.log(reason);
+          callback();
+        });
     });
   });
 
 vorpal
-  .delimiter('prol⚒') //
+  .delimiter('prol⚒')
   .show();
